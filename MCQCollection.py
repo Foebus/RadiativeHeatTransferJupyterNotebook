84ad77697e58dfc459bd73cfa5efdaf6879c8b79
Marc Gay-Balma <m.gaybalmaz@gmail.com>
2020-05-15 00:42:30 +0200

    ADD: Two more mock questions

--- MCQCollection.py
+++ MCQCollection.py
@@ -3,3 +3,11 @@ from InteractionOwnLibrary import MCQ
 
 def testMCQ():
     MCQ("What is your favorite color?", ["Grey", "White", "Black", "Red"], "Red")
+
+
+def testBigMCQ():
+    MCQ("What is The color of Napoleon's white horse?", ["Grey", "Brown"], "Grey")
+
+
+def testLittleMCQ():
+    MCQ("How old is Chewbaka?", ["20", "50", "75", "102", "1032", "42", "No one knows"], "No one knows")

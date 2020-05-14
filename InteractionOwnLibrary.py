11f2d2acabc1d0ccb96859ee88981968065d7400
Marc Gay-Balma <m.gaybalmaz@gmail.com>
2020-05-14 23:09:49 +0200

    ADD: Modularized a lot the simulation creation

---
+++ InteractionOwnLibrary.py
@@ -0,0 +1,63 @@
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, CustomJS, Slider
+from bokeh.plotting import figure
+from bokeh.io import output_notebook, show
+
+
+class VariableDescription:
+    def __init__(self, name, description: str, min_value: float, max_value: float, step_size: float, init_value: float):
+        self.name = name
+        self.description = description
+        self.min_value = min_value
+        self.max_value = max_value
+        self.step_size = step_size
+        self.init_value = init_value
+        self.slider = None
+
+
+class GraphDescription:
+    def __init__(self, variables: [VariableDescription], js_formula,
+                 first_graph_values, x_range, y_range,
+                 description: str, height: int, width: int,
+                 x_label: str, y_label: str):
+        self.variables = variables
+        self.jsFormula = js_formula
+        self.first_graph_values = first_graph_values
+        self.x_range = x_range
+        self.y_range = y_range
+        self.description = description
+        self.height = height
+        self.width = width
+        self.x_label = x_label
+        self.y_label = y_label
+
+
+def create_simulation_from_description(gd: GraphDescription):
+    args_dict = {}
+    slider_list = []
+    for v in gd.variables:
+        v.slider = Slider(title=v.description, value=v.init_value, start=v.min_value,
+                          end=v.max_value, step=v.step_size)
+        args_dict[v.name] = v.slider
+        slider_list.append(v.slider)
+    # Set up the plot
+    x, y = gd.first_graph_values
+    source = ColumnDataSource(data=dict(x=x, y=y))
+    args_dict["source"] = source
+    plot = figure(plot_height=400, plot_width=600, title=gd.description,
+                  tools="pan,wheel_zoom",
+                  x_range=gd.x_range, y_range=gd.y_range)
+
+    plot.line("x", "y", source=source, line_width=3, line_alpha=0.6)
+
+    # Set the js update function
+    update_curve = CustomJS(args=args_dict, code=gd.jsFormula)
+
+    # Set Listeners on the sliders
+    for w in slider_list:
+        w.js_on_change('value', update_curve)
+
+    # Set up layouts and add to document
+    inputs = column(slider_list)
+    layout = row(plot, inputs)
+    show(layout)
---
+++ simulationCollection.py
@@ -0,0 +1,54 @@
+from InteractionOwnLibrary import GraphDescription, VariableDescription
+
+#######################################################################################################################
+###################################### Heat flux graph description creation ###########################################
+#######################################################################################################################
+
+
+def heat_flux_graph_inital_values(A1=1, A2=10, e1=0.4, T1=275, T2=280, emissivity_step_nb=500):
+    points = []
+    x_axis = []
+    sigma = 5.6704 * pow(10, -8)
+    for epsilon2 in range(emissivity_step_nb):
+        e2 = (epsilon2 + 1) / emissivity_step_nb
+        x_axis.append(e2)
+        if A1 < A2:
+            points.append((A1 * sigma * (pow(T1, 4) - pow(T2, 4))) / (1 / e1 + (A1 / A2 * (1 / e2 - 1))))
+        else:
+            points.append(-1)
+    return x_axis, points
+
+
+heat_flux_graph_variables = [VariableDescription("temperature1", "Outer temperature [K]", 275, 350, 0.1, 275),
+                             VariableDescription("temperature2", "Inner temperature [K]", 275, 350, 0.1, 280),
+                             VariableDescription("emissivity1", "Inner ε", 0, 1, 0.002, 0.4),
+                             VariableDescription("area1", "Inner area1 [m²]", 1, 10, 0.1, 1),
+                             VariableDescription("area2", "Outer area [m²]", 1, 10, 0.1, 10)]
+
+heat_flux_graph_formula = """
+        var data = source.data;
+        var A1 = area1.value;
+        var A2 = area2.value;
+        area1.end=A2
+        var e1 = emissivity1.value;
+        var T1 = temperature1.value;
+        var T2 = temperature2.value;
+        var sigma = 5.6704*Math.pow(10, -8);
+        var x = data['x'];
+        var y = data['y'];
+        for (var i = 0; i < 500; i++) {
+            var e2 = (i+1)/500;
+            if (A1 < A2){
+                y[i] = (A1 * sigma * (Math.pow(T1, 4) - Math.pow(T2, 4)))/(1/e1 + (A1/A2 * (1/e2 - 1)));
+            }else{
+                y[i] = -1
+            }
+        }
+
+        // necessary becasue we mutated source.data in-place
+        source.change.emit();
+    """
+
+heat_flux_graph_description = GraphDescription(heat_flux_graph_variables, heat_flux_graph_formula,
+                                               heat_flux_graph_inital_values(), [0, 1], [-10, 0],
+                                               "Heat flux value graph", 400, 600, "Outer ε", "Heat flux value []")

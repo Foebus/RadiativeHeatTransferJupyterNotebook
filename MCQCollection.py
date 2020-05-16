6aa57874c56b9425e999c8437e14e59ce2892057
Tyler Benkley <tylerbenkley@tylers-macbook-pro.home>
2020-05-16 18:09:00 +0200

    Adding changes to nb

--- MCQCollection.py
+++ MCQCollection.py
@@ -11,3 +11,15 @@ def testBigMCQ():
 
 def testLittleMCQ():
     MCQ("How old is Chewbaka?", ["20", "50", "75", "102", "1032", "42", "No one knows"], "No one knows")
+
+def ViewFactorMCQ11():
+    MCQ("What is the F1_1?", ["0","0.25","0.5","0.75","1"],"0")
+    
+def ViewFactorMCQ12():
+    MCQ("What is the F1_2?", ["0","0.25","0.5","0.75","1"],"1")
+    
+def ViewFactorMCQ21():
+    MCQ("What is the F2_1?", ["0","0.25","0.5","0.75","1"],"0.5")
+    
+def ViewFactorMCQ22():
+    MCQ("What is the F2_2?", ["0","0.25","0.5","0.75","1"],"0.5")
